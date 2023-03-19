2fe306f70afe9b42e0cf092c99cf71e0a31ff09f
ItsMarcusLol <marcusaleclee2000@gmail.com>
2023-03-18 18:15:58 -0700

    Fixed bearer token header to add bearer in front of the auth token

--- pkg/httpClientWrapper/createHttpRequest.go
+++ pkg/httpClientWrapper/createHttpRequest.go
@@ -8,7 +8,7 @@ func createHttpRequest(url string, httpMethod string, bearer string) *http.Reque
 	request, _ := http.NewRequest(httpMethod, url, nil)
 
 	if len(bearer) > 0 {
-		request.Header.Add("Authorization", bearer)
+		request.Header.Add("Authorization", "Bearer "+bearer)
 	}
 
 	return request
