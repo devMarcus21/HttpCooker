3b73f254ef7b1da8b3a514b7ceb2ad5c28a7e73c
ItsMarcusLol <marcusaleclee2000@gmail.com>
2023-03-28 22:57:07 -0700

    Adding flag usage descriptions when running --help

--- pkg/main.go
+++ pkg/main.go
@@ -11,14 +11,14 @@ import (
 )
 
 func main() {
-	url := flag.String("url", "", "")
-	httpMethod := flag.String("method", "GET", "")
-	bearerString := flag.String("bearer", "", "")
-	groups := flag.Int("groups", 1, "")
-	sets := flag.Int("sets", 1, "")
-	calls := flag.Int("calls", 0, "")
-	con := flag.Bool("con", false, "")
-	waitAfterGroup := flag.Int("wg", 0, "")
+	url := flag.String("url", "", "Url to make calls to")
+	httpMethod := flag.String("method", "GET", "Http method to make call with")
+	bearerString := flag.String("bearer", "", "Bearer authorization token to make call with")
+	groups := flag.Int("groups", 1, "Number of groups")
+	sets := flag.Int("sets", 1, "Number of sets per group")
+	calls := flag.Int("calls", 0, "Number of HTTP calls to the url per set")
+	con := flag.Bool("con", false, "Make calls concurrently or not")
+	waitAfterGroup := flag.Int("wg", 0, "Wait in seconds to wait between each group")
 
 	flag.Parse()
 
